70f2ec9ab04d00500ffcd13cae3d4309baa77824
YanaYagudina <110576679+YanaYagudina@users.noreply.github.com>
2023-02-01 14:05:31 -0700

    add files

---
+++ README.md
@@ -0,0 +1,20 @@
+Phase-1-Project-Crypto-Tracker
+By: Yana & Mason
+
+-Pitch- Our API holds
+
+Project Requirments
+
+1. Must be HTML/CSS/JS frontend that accesses data from a public API or form a db.josn file using json-server.
+2. Entire app must run on a single webpage. Have 1 HTML file.
+3. Must use 3 different event listeners that enable interactivity. Each event listener should have its own unique callback function.
+4. Must implement at least 1 instance of array iteration, using any method.
+5. Follow good coding practices, keep code DRY.
+Phase-1-Project
+to do:
+
+1. fetch List of Coins https://coinmap.org/api/#api-Coins-GetCoins
+2. create table
+3. event favorite (change color on the star)
+4. event additional info as allert event
+5. stripe forms
---
+++ index.js
@@ -0,0 +1,190 @@
+const coinList = document.getElementById('list') // -> change it for table places
+
+const stripe = Stripe("pk_test_51LpDh5GTck0sdjJJB2jBz7La6RAo7wJrML0EEenMPzyNdHVGGgxv8kc5vEv9Wy9yy9wjY7AIEnz4Jo2zj8Tgdjvv00SyIxzIDb");
+
+//iFrame
+const appearance = {
+    theme: 'default',
+};
+elements = stripe.elements({ appearance, clientSecret: 'pi_3LpN6OGTck0sdjJJ3qk2PDzl_secret_PZ4EI0W264HiqirIrVmTOUE06' });
+const paymentElement = elements.create("payment");
+paymentElement.mount("#payment-element");
+
+// -> show Curent Date
+
+// document.getElementById("CurentDateBtn").addEventListener("click", displayDate);
+// function displayDate() {
+//   document.getElementById("CurentDate").innerHTML = Date();
+// }
+
+
+//-> mouse out/mouse over
+var x = document.getElementById("CurentDateBtn");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("click", displayDate);
+x.addEventListener("mouseout", myThirdFunction);
+function myFunction() {
+    document.getElementById("CurentDateBtn").innerHTML = "Click to Show Date<br>";
+}
+
+function myThirdFunction() {
+    document.getElementById("CurentDateBtn").innerHTML = "Hide Date<br>";
+}
+function displayDate() {
+    document.getElementById("CurentDateBtn").innerHTML += Date();
+}
+
+
+
+// -> search element
+// -> TODO add search button
+//const coinImput = 
+// document.getElementsByClassName('text').onsearch = function() {
+//     myFunction ()
+// }
+// myFunction () { 
+
+
+// }
+// //document.getElementById("myInput").onsearch = function() {myFunction()};
+
+// function myFunction() {
+//   var x = document.getElementById("myInput");
+//   document.getElementById("demo").innerHTML = "You are searching for: " + x.value;
+// }
+
+//API coin
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&fbclid=IwAR2seyvVJZbkupG8ZP75m6uh_cqGH5NeTLcq2l4v7cAPKCPF_jh-_Kvc9dg")//, {mode: "no-cors"})
+    // TODO -> fetch(('http://localhost:3000/market'))
+    .then(Response => Response.json())
+    .then(coinArr => {
+        for (let i = 0; i < 10; i++) {
+            displayInfo(coinArr[i])
+
+        }
+        // .then(coinArr => console.log(coinArr))
+    })
+
+
+
+//coinObj.data.forEach(x => console.log(x))
+// .then(coinArr => console.log(coinArr))
+
+function displayInfo(coinObj) {
+    // -> console.log(coinObj)
+    //-> doesn't work: console.log(coinObj[0])
+    // console.log(coinObj.name)
+    // console.log(coinObj.image)
+
+    //->same as 
+    //console.log(coinObj.name, coinObj.current_price, coinObj.image, coinObj.symbol)
+
+    // check it on w3schools: for in, for of
+    // for (x in obj)
+    // x- is key
+    const tr = document.createElement('tr')
+    tr.className = "coinInfo"
+
+    // -> added star img. created Favorit column
+    td = document.createElement('td')
+    tr.append(td)
+    let emptyStar = document.createElement('img')
+    emptyStar.src = './img/empty_star.png'
+    // -> td.setAttribute('id', 'starButton')
+    emptyStar.className = "emptyStar"
+    td.id = 'starButton'
+    td.append(emptyStar)
+    // -> Double click event and click event
+    emptyStar.addEventListener("dblclick", () => {
+        emptyStar.src = './img/empty_star.png'
+        emptyStar.src = './img/filled_star.png'
+        console.log("empty")
+    })
+    emptyStar.addEventListener("click", () => {
+        emptyStar.src = './img/filled_star.png'
+        emptyStar.src = './img/empty_star.png'
+        console.log("emptyfiled")
+    })
+
+    // -> added coin Img
+    td = document.createElement('td')
+    tr.append(td)
+    const coinImg = document.createElement('img')
+    coinImg.className = "logo"
+    td.append(coinImg)
+    coinImg.src = coinObj.image
+
+    // -> added symbol
+    td = document.createElement('td')
+    tr.append(td)
+    td.textContent = coinObj.symbol
+
+    // -> added coin Name
+    td = document.createElement('td')
+    tr.append(td)
+    td.textContent = coinObj.name
+
+    // -> added prices
+    td = document.createElement('td')
+    tr.append(td)
+    td.textContent = `$ ${coinObj.current_price}`
+
+    // -> added percantage
+    td = document.createElement('td')
+    tr.append(td)
+    td.textContent = coinObj.price_change_percentage_24h
+    coinList.append(tr);
+    // -> input number
+    td = document.createElement('td')
+    tr.append(td)
+    const coinInput = document.createElement('input')
+    coinInput.id = "textBox"
+    coinInput.type = "number"
+    td.append(coinInput)
+}
+
+
+
+
+
+//-> keydown
+
+// let btn = document.createElement("button");
+// btn.innerHTML = "Submit";
+// btn.type = "submit";
+// btn.name = "formBtn";
+// document.body.appendChild(btn);
+
+// let coinLine = document.getElementsByClassName("coinInfo").createElement("button")
+// coinLine.addEventListener('click', () => {
+//     console.log('work')
+// })
+
+
+
+document.getElementById("payment-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+})
+// -> mouseover event reset payment
+ducument.getElementById("myCheck").addEventListener('mouseover', () => {
+    document.getElementById("myCheck").click();
+    setLoading(false)
+}
+)
+
+
+// Show a spinner on payment submission
+function setLoading(isLoading) {
+    if (isLoading) {
+        // Disable the button and show a spinner
+        document.querySelector("#submit").disabled = true;
+        document.querySelector("#spinner").classList.remove("hidden");
+        document.querySelector("#button-text").classList.add("hidden");
+    } else {
+        document.querySelector("#submit").disabled = false;
+        document.querySelector("#spinner").classList.add("hidden");
+        document.querySelector("#button-text").classList.remove("hidden");
+    }
+}
